8d189b176294e65c97be592cf0a630b217ba73ed
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-01-19 20:15:06 -0300

    fix: correct footer text from 'Gera Lead' to 'Mega Lead'

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="flex items-center justify-center text-sm h-14  lg:h-10 bg-[#a3a3a3] ">
       <p className="text-neutral-300 text-center text-[13px] ">
-        Gera Lead - 2026
+        Mega Lead - 2026
       </p>
     </footer>
   );
