881f1f5f9dd3a062a442d23cf501109e1dab6d36
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-01-19 20:12:30 -0300

    refactor: update footer background color for consistency

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="flex items-center justify-center text-sm h-14  lg:h-10 bg-[#797b7c] ">
+    <footer className="flex items-center justify-center text-sm h-14  lg:h-10 bg-[#a3a3a3] ">
       <p className="text-neutral-300 text-center text-[13px] ">
         Gera Lead - 2026
       </p>
